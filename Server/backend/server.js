7af6de6630149862814e144eca5f489fba720191
Khanjan <khanjanvarma9@gmail.com>
2021-06-24 14:02:52 +0530

    Few changes

--- Server/backend/server.js
+++ Server/backend/server.js
@@ -23,6 +23,8 @@ app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
+app.get('/', (req, res) => { res.send('Hello from Express!')});
+
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
     const product = data.products.find(x => x._id === productId);
@@ -36,4 +38,4 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(process.env.PORT || 5000, () => { console.log("Server started at http://localhost:5000") });
