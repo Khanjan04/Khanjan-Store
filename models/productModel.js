a53a3e02baf26e72f734e161314a92df7e0f311d
Khanjan <khanjanvarma9@gmail.com>
2021-06-26 09:17:03 +0530

    Deployment to Heroku

--- models/productModel.js
+++ models/productModel.js
@@ -10,7 +10,6 @@ const productSchema = new mongoose.Schema({
     description: { type: String, required: true},
     rating: { type: Number, default: 0, required: true},
     numReviews: { type: Number, default: 0, required: true}
-     
 });
 
 const productModel = mongoose.model("Product", productSchema);
