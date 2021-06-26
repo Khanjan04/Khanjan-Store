bfa880fe6a19e09857493c8d9cef9a7a77f5ed41
Khanjan <khanjanvarma9@gmail.com>
2021-06-27 02:56:58 +0530

    Solving error of Heroku

--- server.js
+++ server.js
@@ -52,4 +52,4 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(process.env.PORT || 5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(process.env.PORT || 4000, () => { console.log("Server started at http://localhost:4000") });
