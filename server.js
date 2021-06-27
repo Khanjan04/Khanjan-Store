5d7abda7f31f89e0b8fabcf1f56d029632937dc5
Khanjan <khanjanvarma9@gmail.com>
2021-06-28 00:17:05 +0530

    Few changes

--- server.js
+++ server.js
@@ -52,4 +52,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(process.env.PORT || 5000, () => { console.log("Server started at http://localhost:5000") });
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => { console.log('Server started on', port) });
